8033576ffae17953d2286e56bc40b33aa6d37d27
ancasimirnicolas <102700431+ancasimirnicolas@users.noreply.github.com>
2022-05-21 17:54:57 -0400

    Files

---
+++ mkdir lesson_two/Handsonlesson2.py
@@ -0,0 +1,32 @@
+day = "17"
+year = "1983"
+month = "February"
+
+my_birthday = month + " " + day + ", " + year
+
+print (my_birthday)
+
+first = "happy"
+second = "birthday"
+third = "to"
+fourth = "you"
+
+final = first+" " + second + " "+ third+" " + fourth
+print (final)
+
+age = 15
+
+if age >=18 : 
+   print ("Permitted to attend alone") 
+
+elif age<10 :
+    print("Not permitted")
+
+elif age<15 :
+    print("Permitted with a parent")
+
+elif age<18 :
+    print("Permitted with anyone over 18")
+    
+
+
---
+++ mkdir lesson_two/main.py
@@ -0,0 +1,23 @@
+my_message = "Hello"
+my_other_message = "how are you?"
+
+# concatenate the above variables with a comma in between
+my_final_message = my_message + ", " + my_other_message
+
+print(my_final_message)
+
+day = "1"
+year = "2017"
+month = "August"
+
+today_is = month + " " + day + ", " + year
+
+print(today_is)
+
+day = 1
+year = 2017
+month = "August"
+
+today_is = month + " " + str(day) + ", " + str(year)
+
+print(today_is)
